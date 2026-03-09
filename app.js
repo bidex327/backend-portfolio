252b929791003815100fb59f090cc761dfcc700e
bidex327 <adebayobidemi303@gmail.com>
2026-03-09 16:21:12 +0100

    Update

--- app.js
+++ app.js
@@ -6,21 +6,20 @@ const messageRoutes = require("./routes/messageRoutes");
 
 const app = express();
 
-// Connect to MongoDB
+
 connectDB();
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Routes
+
 app.use("/api/messages", messageRoutes);
 
-// Test route
+
 app.get("/", (req, res) => {
   res.send("welcome to my api");
 });
 
-// ✅ IMPORTANT: Export app for Vercel
 module.exports = app;
